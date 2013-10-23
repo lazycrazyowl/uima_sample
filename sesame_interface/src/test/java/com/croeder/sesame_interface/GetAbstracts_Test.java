e12fc7d715bd1988a983b6f432f0a267bb6405a6
Chris Roeder <Chris.Roeder@ucdenver.edu>
2013-10-23 15:55:25 -0600

    basic GetAbstracts test

--- sesame_interface/src/test/java/com/croeder/sesame_interface/GetAbstracts_Test.java
+++ sesame_interface/src/test/java/com/croeder/sesame_interface/GetAbstracts_Test.java
@@ -6,24 +6,94 @@ import org.junit.Before;
 import org.junit.After;
 
 import java.io.File;
+import java.util.List;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.Statement;;
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.vocabulary.RDF;
+
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.Binding;
+import org.openrdf.query.Update;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
 
 public class GetAbstracts_Test {
-	GetAbstracts myClass;
+	Logger logger = Logger.getLogger(GetAbstracts_Test.class);
+	GetAbstracts ga;
 
 	@Before
 	public void setup() throws Exception { 
-		//myClass.preliminaryStuff();
-		myClass = new GetAbstracts();
-	}
+		//ga = new GetAbstracts("conn.sail");
+		ga = new GetAbstracts("conn.ag");
 
-	@Test
-	public void test_1() {
-		//String s = myClass.doStuff(new File("test.txt"));
-		//assertEquals("the returned string should be from the file", "This is a file at the top level.", s);
+
+		ga.deleteBatches();
+
+		ga.createSets();
 	}
 
 	@After
-	public void teardown() {
-		//myClass.close();
+	public void teardown() throws Exception {
+		ga.deleteBatches();
+		ga.close();
 	}
+
+	@Test
+	public void test_getPmidsBatch_1() {
+		List<URI> list = ga.getPmidsBatch(0);  
+
+		logger.info("test 1 batch size " + list.size());
+
+		assertEquals("the batches should be full-size", 100, list.size());
+		logger.info(list.get(0));
+	}
+
+	//@Test
+	public void test_getPmidsBatch_4() {
+		List<URI> list = ga.getPmidsBatch(1);  
+		logger.info("test 1 batch size " + list.size());
+		assertEquals("the batches should be full-size", 100, list.size());
+	}
+
+
+	//@Test
+	public void test_getPmidsBatch_2() {
+		List<URI> list = ga.getPmidsBatch(99);  
+		logger.info("test 1 batch size " + list.size());
+		assertEquals("the batches should be full-size", 100, list.size());
+	}
+
+	//@Test
+	public void test_getPmidsBatch_3() {
+		List<URI> list = ga.getPmidsBatch(100);  
+		logger.info("test 1 batch size " + list.size());
+		assertEquals("the batches should be full-size", 0, list.size());
+	}
+
+	//@Test
+	public void test_getAbstract_1() {
+		String a = ga.getAbstract("bogus");
+	}
+
+	//@Test
+	public void test_getAbstract_2() { 
+		///String a = getAbstract(Value medlineUri);
+	}
+
+
 }
